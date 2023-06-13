1cb7ea24235f58f1f7821c8efc8a50d88d3f1ab9
Lisset Murcia <missyanith@gmail.com>
2023-06-13 13:14:08 -0400

    Solved problems

---
+++ src/main/java/dataStructures/queuesAndStacks/Solution.java
@@ -0,0 +1,68 @@
+package dataStructures.queuesAndStacks;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/*
+Problem: https://www.hackerrank.com/challenges/30-queues-stacks/
+ */
+public class Solution {
+    private Stack stack;
+    private Queue queue;
+
+    public Solution() {
+        stack = new Stack<>();
+        queue = new LinkedList();
+    }
+
+    public void pushCharacter(char c) {
+        stack.push(c);
+    }
+
+    public void enqueueCharacter(char c) {
+        queue.add(c);
+    }
+
+    public char popCharacter() {
+        char c = (char) stack.pop();
+        return c;
+    }
+
+    public char dequeueCharacter() {
+        char c = (char) queue.poll();
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Solution p = new Solution();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length / 2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println("The word, " + input + ", is "
+                + ((!isPalindrome) ? "not a palindrome." : "a palindrome."));
+    }
+}
