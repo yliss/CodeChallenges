5ba5cfdb281236a43e61a29456f54d80ee320324
Lisset Murcia <missyanith@gmail.com>
2023-06-13 14:55:47 -0400

    Solved problems

---
+++ src/main/java/problemSolving/SortingBubbleSort/Solution.java
@@ -0,0 +1,54 @@
+package problemSolving.SortingBubbleSort;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.Collections.swap;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+        int accnumberOfSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a.get(j) > a.get(j + 1)) {
+                    int nLeft= a.get(j);
+                    int nRight= a.get(j + 1);
+                    a.set(j + 1, nLeft );
+                    a.set(j,nRight);
+                    numberOfSwaps++;
+                }
+            }
+            accnumberOfSwaps+=numberOfSwaps;
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in " + accnumberOfSwaps + " swaps.");
+        System.out.println("First Element: "+ a.get(0));
+        System.out.println("Last Element: "+a.get(a.size()-1));
+        bufferedReader.close();
+    }
+}
